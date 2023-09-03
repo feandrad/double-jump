efac487126307a51838c4aa90a29cec225f36f86
feandrad <feandrad@gmail.com>
2023-09-03 15:23:51 -0300

    1.20.1 update

--- src/main/java/io/felipeandrade/doublejump/item/Cabbage.java
+++ src/main/java/io/felipeandrade/doublejump/item/Cabbage.java
@@ -4,16 +4,13 @@ import io.felipeandrade.doublejump.DoubleJumpMod;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 
 public class Cabbage extends Item {
     public Cabbage() {
-        super(new Item.Settings()
-                .group(ItemGroup.FOOD)
-                .food(new FoodComponent.Builder()
-                        .hunger(3) // Adjust the hunger value as desired
-                        .saturationModifier(0.6F)
-                        .statusEffect(new StatusEffectInstance(DoubleJumpMod.FLATULENCE_EFFECT, 600, 0), 1.0F)
-                        .build()));
+        super(new Item.Settings().food(new FoodComponent.Builder()
+                .hunger(3) // Adjust the hunger value as desired
+                .saturationModifier(0.6F)
+                .statusEffect(new StatusEffectInstance(DoubleJumpMod.FLATULENCE_EFFECT, 600, 0), 1.0F)
+                .build()));
     }
 }
